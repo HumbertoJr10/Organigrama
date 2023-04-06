74ce739b7d219784fc5171a72610269aa42e8bbe
Humberto JR <humbertojrguerra@gmail.com>
2023-04-06 13:28:39 -0400

    Agregando Redux para manejar el estado, formateando estilos

---
+++ src/redux/reducer.js
@@ -0,0 +1,18 @@
+
+// ----> Estado Inicial <----
+const initialState = {
+    team: [],
+    teamRespaldo: []
+}
+
+// ---> REDUCER <-----
+
+const reducer = (state = initialState, { type, payload }) => {
+    switch (type) {
+  
+        default:
+            return {...state}
+    }
+}
+
+export default reducer
