e92ad7c976f3a25cc22955d04c870919707dac29
Humberto JR <humbertojrguerra@gmail.com>
2023-04-06 21:22:12 -0400

    clean code

--- src/helper/function.js
+++ src/helper/function.js
@@ -14,15 +14,4 @@ export function removeDuplicates(arr) {
     }
     return unicos
 
-    // const recientes = arr.filter( e => e["Mes"][0] == 5)
-
-    // const result = [];
-    // const ids = new Set(); // Conjunto para almacenar los IDs únicos
-    // for (const obj of recientes) {
-    //   if (!ids.has(obj.ID)) { // Si el ID no ha sido agregado previamente
-    //     ids.add(obj.ID); // Agregar el ID al conjunto
-    //     result.push(obj); // Agregar el objeto al resultado
-    //   }
-    // }
-    // return result;
   }
